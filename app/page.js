becf5993cff72151cb845b1d29ae8b853d078b1d
mohammadyazarloo2 <mohammadyazarloo2@gmail.com>
2024-04-19 16:59:25 +0330

    fix callback

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  if (session) redirect("/dashboard");
+  // if (session) redirect("/dashboard");
 
   return (
     <main>
